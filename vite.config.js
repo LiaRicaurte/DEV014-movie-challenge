cb94ed8150713289701fa7efa9ed0b18a0aa9ab7
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-06-28 14:32:06 -0500

    agrega nuevamente el archivo vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      root: 'src',
+      // para hacer un deploy, configura propiedad base con el nombre/url de tu repo
+      // https://vitejs.dev/guide/static-deploy.html#github-pages
+      // ejemplo:
+      build: {
+        minify: false,
+        rollupOptions: {
+          output: {
+            dir: './dist',
+          },
+        },
+        // sourcemap: true,
+      },
+    };
+  }
+  return {};
+});
